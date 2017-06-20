0cb95105f61945bdcc500ce7b40fecfec16920d8
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-20 15:49:54 -0400

    Make from and style it, handle form input in state

--- src/Github.js
+++ src/Github.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
+import './Github.css'
+
+import { Route } from 'react-router-dom'
 
 class Github extends Component {
+    state = {
+        username: '',
+    }
+
+    handleChange = (e) => {
+        const username = e.currentTarget.value
+        this.setState({ username })
+    }
+
     render(){
         return (
-            <h1>Github Component</h1>
+            <div className="github">
+                <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github logo" />
+                <form>
+                    <div>
+                        <input 
+                            type="text"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Look up Github user</button>
+                    </div>
+
+                </form>
+
+                <Route exact path='/github' render={() => <h3>Please enter a username to search on GitHub</h3>} />
+            </div>
         )
     }
 }
