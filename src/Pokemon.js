c2afe21eacb274b4021577ff7faa7c36b96fa1b1
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-20 17:55:03 -0400

    Add form and logo to pokemon component and style

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react'
 import './Pokemon.css'
 
 class Pokemon extends Component {
+    state = {
+        pokeName: '',
+    }
+
+    handleChange = (e) => {
+        const pokeName = e.currentTarget.value
+        this.setState({ pokeName })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.history.push(`/pokemon/${this.state.pokeName}`)
+    }
+
     render(){
         return(
-            <div>
-                <h1> POKÉMON!!! </h1>
+            <div className="pokemon">
+                <img className="pokemon-logo" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pokémon_logo.svg/2000px-International_Pokémon_logo.svg.png" 
+                    alt="pikachu" />
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input 
+                            type="text"
+                            value={this.state.pokeName}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Look up a Pokémon!</button>
+                    </div>
+                </form>
             </div>
         )
     }
