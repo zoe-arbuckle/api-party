7f6fec8ec1b9b7fd9b74035bf4484e54d8917322
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-20 16:07:34 -0400

    Fetch user data from Github API

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component{
+    constructor(props){
+        super(props)
+
+        this.fetchUserData()
+    }
+
+    fetchUserData = () => {
+        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => console.log(user))
+    }
+
     render(){
         return(
             <p>Github User</p>
