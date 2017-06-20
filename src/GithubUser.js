dc0b6f3db04a664eff3d32ff107ec09ff267ea56
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-20 16:16:27 -0400

    Add markup for Github User and style

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
+import './GithubUser.css'
 
 class GithubUser extends Component{
+    state = {
+        user: {
+            avatar_url: '',
+            login: '',
+            followers: '',
+            following: '',
+            location: '',
+            html_url: '',
+        },
+    }
+
     constructor(props){
         super(props)
 
@@ -10,12 +22,20 @@ class GithubUser extends Component{
     fetchUserData = () => {
         fetch(`https://api.github.com/users/${this.props.match.params.username}`)
             .then(response => response.json())
-            .then(user => console.log(user))
+            .then(user => this.setState({ user }))
     }
 
     render(){
+        const { user } = this.state
         return(
-            <p>Github User</p>
+            <div className = "github-user">
+                <img src={user.avatar_url} alt="" />
+                <h2>{user.login}</h2>
+                <h3>followers: {user.followers}</h3>
+                <h3>following: {user.following}</h3>
+                <h3>location: {user.location}</h3>
+                <a href={user.html_url}>Link to {user.login}'s profile </a>
+            </div>
         )
     }
 }
