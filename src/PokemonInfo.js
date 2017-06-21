3366945d8262a23d00acd4d2a87353ec09b039e2
Zoe Arbuckle <zoe.arbuckle@me.com>
2017-06-21 00:05:34 -0400

    Add a button to pokemon info and handle case that pokemon does not exist in API

--- src/PokemonInfo.js
+++ src/PokemonInfo.js
@@ -44,29 +44,45 @@ class PokemonInfo extends Component {
     }
 
     hasSecondaryType = () => {
-        if(this.state.pokemon.types.length > 1){
+        if (this.state.pokemon.types.length > 1) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
+    handleClick = () => {
+        
+    }
+
     render() {
         const { pokemon } = this.state
-        let hStr = pokemon.height.toString()
-        let wStr = pokemon.weight.toString()
-        hStr = hStr.substr(0, hStr.length - 1) + '.' + hStr.substr(hStr.length - 1)
-        wStr = wStr.substr(0, wStr.length - 1) + '.' + wStr.substr(wStr.length - 1)
-        return (
-            <div className={`pokemon-info ${pokemon.types[0].type.name}`}>
-                <img src={pokemon.sprites.front_default} alt="sprite" />
-                <h2>{pokemon.name.toUpperCase()}</h2>
-                <h2>type: {pokemon.types[0].type.name}</h2>
-                {this.hasSecondaryType() ? <h2>secondary type: {pokemon.types[1].type.name}</h2> : null}
-                <h4>height: {hStr} m</h4>
-                <h4>weight: {wStr} kg</h4>
-            </div>
-        )
+        if (pokemon.height) {
+            let hStr = pokemon.height.toString()
+            let wStr = pokemon.weight.toString()
+            hStr = hStr.substr(0, hStr.length - 1) + '.' + hStr.substr(hStr.length - 1)
+            wStr = wStr.substr(0, wStr.length - 1) + '.' + wStr.substr(wStr.length - 1)
+            return (
+                <div className={`pokemon-info ${pokemon.types[0].type.name}`}>
+                    <img src={pokemon.sprites.front_default} alt="sprite" />
+                    <h2>{pokemon.name.toUpperCase()}</h2>
+                    <div>
+                        {this.hasSecondaryType()
+                            ? <h2>{pokemon.types[0].type.name}/{pokemon.types[1].type.name}</h2>
+                            : <h2>{pokemon.types[0].type.name}}</h2>}
+                    </div>
+                    <h4>height: {hStr} m</h4>
+                    <h4>weight: {wStr} kg</h4>
+                    <div>
+                        <button type="onClick" onClick={this.handleClick}
+                            className={`${pokemon.types[0].type.name}`}>Display Evolutions
+                        </button>
+                    </div>
+                </div>
+            )
+        } else {
+            return <h3>That was not a valid pokemon. Please enter a number between 1 and 721.</h3>;
+        }
     }
 }
 
